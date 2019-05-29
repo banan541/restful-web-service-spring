80621d98b6fe877bf22b1d4464e035d3dff15b8e
banan541 <banan541@gmail.com>
2019-05-29 18:16:14 +0200

    Add CrossOrigin and exception handling

--- src/main/java/com/bananProject/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/bananProject/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,11 +1,12 @@
 package com.bananProject.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class HelloWorldController {
 
@@ -15,6 +16,7 @@ public class HelloWorldController {
 	}
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
+//		throw new RuntimeException("Some Error has Happened! Contact support at ***_***");
 		return new HelloWorldBean("Hello World");
 	}
 	
